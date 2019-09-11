fed74e23c7386d68205c554c0cd271e4769b8851
viktorlandstrom <viktor.landstrom@umea.ntig.ga.se>
2019-09-11 11:07:48 +0200

    swag

---
+++ js/main.js
@@ -0,0 +1,10 @@
+function randomizeColor(){
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) 
+    color += letters[(Math.floor(Math.random() * 16))]; 
+    document.body.style.background = color;
+}
+function resetColor(){
+    document.body.style.background = '#FFFFFF';
+}
